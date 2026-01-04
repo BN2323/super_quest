72be1b06576b734ef43873b1aeffff73ad3a1930
BN2323 <Bunnaroth.tha@student.cadt.edu.kh>
2026-01-04 15:36:37 +0700

    set up presentation layer architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:super_quest/data/mock/mock_game_data.dart';
+import 'package:super_quest/domain/services/challenge_service.dart';
+import 'package:super_quest/domain/services/game_service.dart';
+import 'package:super_quest/presentation/controllers/game_controller.dart';
+import 'package:super_quest/presentation/screens/app_entry.dart';
 
 void main() {
   runApp(const SuperQuestApp());
@@ -9,10 +15,21 @@ class SuperQuestApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SuperQuest',
-      debugShowCheckedModeBanner: false,
-      home: const Placeholder(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => GameController(
+            challengeService: ChallengeService(),
+            gameService: GameService(),
+            dungeon: MockGameData.dungeon,
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'SuperQuest',
+        home: const AppEntry(),
+      ),
     );
   }
 }
