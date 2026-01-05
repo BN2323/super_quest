47021b64a7fa161f23eab4d838ba994adfe226b3
BN2323 <Bunnaroth.tha@student.cadt.edu.kh>
2026-01-05 14:49:43 +0700

    refactor: move solution validation logic into Challenge model

--- lib/domain/models/challenge.dart
+++ lib/domain/models/challenge.dart
@@ -1,17 +1,33 @@
 import 'code_block.dart';
 
+enum ChallengeType {
+  blockAssembly,
+}
+
 class Challenge {
   final String id;
+  final ChallengeType type;
   final String description;
+
   final List<CodeBlock> availableBlocks;
   final List<String> expectedBlockOrder;
   final int maxAttempts;
 
   Challenge({
     required this.id,
+    required this.type,
     required this.description,
     required this.availableBlocks,
     required this.expectedBlockOrder,
     required this.maxAttempts,
   });
+
+  bool isCorrect(List<CodeBlock> userBlocks) {
+    if (userBlocks.length != expectedBlockOrder.length) return false;
+
+    for (int i = 0; i < expectedBlockOrder.length; i++) {
+      if (userBlocks[i].id != expectedBlockOrder[i]) return false;
+    }
+    return true;
+  }
 }
